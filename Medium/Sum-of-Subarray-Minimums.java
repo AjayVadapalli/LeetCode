fc9dc62dc2e64b969725fd5185969697dbc13857
Vadapalli Ajay Siva Surya <166938304+AjayVadapalli@users.noreply.github.com>
2025-09-02 21:28:52 +0530

    [907] [Time Beats: 79.92%] [Memory Beats: 82.46%] - LeetPush

---
+++ Medium/Sum-of-Subarray-Minimums.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int MOD = (int)1e9 + 7;
+        int n = arr.length;
+        
+        int[] left = new int[n];   
+        int[] right = new int[n];  
+        
+        Stack<int[]> st1 = new Stack<>(); 
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            while (!st1.isEmpty() && st1.peek()[0] > arr[i]) {
+                count += st1.pop()[1];
+            }
+            left[i] = count;
+            st1.push(new int[]{arr[i], count});
+        }
+        
+        Stack<int[]> st2 = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            int count = 1;
+            while (!st2.isEmpty() && st2.peek()[0] >= arr[i]) {
+                count += st2.pop()[1];
+            }
+            right[i] = count;
+            st2.push(new int[]{arr[i], count});
+        }
+        
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = (ans + (long)arr[i] * left[i] * right[i]) % MOD;
+        }
+        
+        return (int)ans;
+    }
+}
