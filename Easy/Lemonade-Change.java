e3d8b78c4c7872a57c5c2e02db7678f8bea71af3
Vadapalli Ajay Siva Surya <166938304+AjayVadapalli@users.noreply.github.com>
2025-12-11 22:30:56 +0530

    [860] [Time Beats: 5.84%] [Memory Beats: 22.06%] - LeetPush

--- Easy/Lemonade-Change.java
+++ Easy/Lemonade-Change.java
@@ -1,33 +1,28 @@
-class Solution {
-    public boolean lemonadeChange(int[] bills) {
-        int n= bills.length;
-        int f=0,t=0,p=0;
-        boolean x=true;
-        for(int i=0;i<n;i++){
-            if(bills[i]==5){
-                f++;
-            }
-            if(bills[i]==10){
-                if(f>=1){
-                    f--;
-                    t++;
-                }else{
-                    x=false;
-                }
-            }
-            if(bills[i]==20){
-                if(f>=1 && t>=1){
-                    f--;
-                    t--;
-                }
-                else if(f>=3){
-                    f-=3;
-                }
-                else{
-                    x=false;
-                }
-            }
-        }
-        return x;
-    }
-}
+1class Solution {
+2    public boolean lemonadeChange(int[] bills) {
+3        int five=0;
+4        int ten=0;
+5        boolean bool=true;
+6        for(int i=0;i<bills.length;i++){
+7            if(bills[i]==5) five+=1;
+8            else if(bills[i]==10){
+9                if(five>=1){
+10                    five--;
+11                    ten++;
+12                }
+13                else bool= false;
+14            }
+15            else{
+16                if(five>=1 && ten>=1){
+17                    five--;
+18                    ten--;
+19                }
+20                else if(five>=3){
+21                    five-=3;
+22                }
+23                else bool= false;
+24            }
+25        }
+26        return bool;
+27    }
+28}
